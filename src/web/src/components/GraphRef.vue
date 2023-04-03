b155290cafb222f04b47e5b7a0cb4274c9a2d5f6
gundam1945 <gundam1945@hotmail.com>
2023-04-03 11:37:53 +0800

    Tried reusing chart in vuejs. Turns out it will result in too much revursion

--- src/web/src/components/GraphRef.vue
+++ src/web/src/components/GraphRef.vue
@@ -1,7 +1,18 @@
+// Tried reusing chart in vuejs. Turns out it will result in too much revursion if changing the data.
+
 <script>
 import Chart from 'chart.js/auto'
 
-const data = [
+export default {
+  data () {
+    return {
+      chart: null
+    }
+  }
+  ,mounted () {
+    const ctx = this.$refs.graph
+
+    const data = [
       { year: 2010, count: 10 },
       { year: 2011, count: 20 },
       { year: 2012, count: 15 },
@@ -11,29 +22,49 @@ const data = [
       { year: 2016, count: 28 },
     ];
 
-export default {
-  props: {
-    id: String,
-    data: Object
-  },
-  mounted () {
-
-    const ctx = this.$refs.graph
-    new Chart(
+    this.chart = new Chart(
       ctx,
       {
         type: 'bar',
         data: {
-          labels: this.fakedata,
+          labels: data.map(row => row.year),
           datasets: [
             {
               label: 'Acquisitions by year',
-              data: this.fakedata
+              data: data.map(row => row.count)
             }
           ]
         }
       }
     );
+  }
+  ,methods: {
+    serveData () {
+      console.log("TEST")
+      // const fakeData = [
+      //   { year: 2010, count: Math.random() },
+      //   { year: 2011, count: Math.random() },
+      //   { year: 2012, count: Math.random() },
+      //   { year: 2013, count: Math.random() },
+      //   { year: 2014, count: Math.random() },
+      //   { year: 2015, count: Math.random() },
+      //   { year: 2016, count: Math.random() },
+      // ];
+
+      // this.labels = fakeData.map(row => row.year)
+      // this.data = fakeData.map(row => row.count)
+
+      this.chart.data.labels.pop()
+      this.chart.data.datasets.forEach(dataset => dataset.data.pop())
+
+      // this.chart.data.labels.push(this.labels)
+      // this.chart.data.datasets = [{
+      //   data: this.data
+      // }]
+
+      console.log("BEFORE UPDATE")
+      this.chart.update()
+    }
   }
 }
 </script>
@@ -41,6 +72,7 @@ export default {
 <template>
   <div>
     <canvas ref="graph"></canvas>
+    <button @click="serveData">TEST</button>
   </div>
 </template>
 
